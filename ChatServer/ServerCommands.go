90f5be5a517c378999e9cf3e505653eb1b59f66b
Adam Pach <adampach9@gmail.com>
2022-02-02 17:02:52 +0100

    Remove room from server

--- ChatServer/ServerCommands.go
+++ ChatServer/ServerCommands.go
@@ -20,9 +20,38 @@ func (s *Server) ManageRooms(reader *bufio.Reader) error {
 		s.rooms[name] = CreateRoom(name)
 		return nil
 	} else if Shared.FormatCommand(command) == "delete" {
-
+		s.PrintAllRooms()
+		rn, err := reader.ReadString('\n')
+		Shared.LogError(&err)
+		err = s.DeleteRoom(Shared.RemoveSendingCharacters(rn))
+		if err != nil {
+			return err
+		}
 		return nil
 	}
 
 	return errors.New("You enter bad choice")
+}
+
+func (s *Server) PrintAllRooms() {
+	fmt.Println("All rooms: ")
+	for r := range s.rooms {
+		fmt.Println(r)
+	}
+}
+
+func (s *Server) DeleteRoom(roomName string) error {
+	deletedRoom := s.rooms[roomName]
+
+	if deletedRoom == nil {
+		return errors.New("You enter bad room name!")
+	}
+
+	for _, client := range deletedRoom.Clients {
+		client.room = nil
+	}
+	delete(s.rooms, deletedRoom.roomName)
+	deletedRoom = nil
+
+	return nil
 }
